756c9e84eb033462a7bc2b02c774c11d41ccd895
tharun <thulasitharun1@gmail.com>
2024-06-17 23:19:33 +0530

    Initial Commit 2

---
+++ AdvancedJavaTelusko/src/com/telusko/Collections/ArrayListDemo.java
@@ -0,0 +1,29 @@
+package com.telusko.Collections;
+
+import java.util.ArrayList;
+import java.util.Collection;
+
+public class ArrayListDemo {
+
+	public static void main(String[] args) {
+		
+		Collection<Integer> nums = new ArrayList<Integer>();
+		for(int i=1;i<=10;i++)
+		{
+			nums.add(i);
+		}
+		//no get method for collection interface
+		//have in child classes
+		System.out.println(nums);
+		
+		ArrayList<Integer> nums1 = new ArrayList<Integer>();
+		for(int i=1;i<=10;i++)
+		{
+			nums1.add(i);
+		}
+		
+		System.out.println(nums1);
+		System.out.println(nums1.get(2));
+	}
+
+}
